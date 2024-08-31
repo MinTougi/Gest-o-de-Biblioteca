93332c496a2cfa9f005ec90e40f126990abf9410
MinTougi <rafaela.gaievski30@gmail.com>
2024-08-31 17:09:03 -0300

    User Menu
    Added a menu for the user

--- Gestao Biblioteca/src/App.java
+++ Gestao Biblioteca/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         
+    Scanner scanner = new Scanner (System.in);
+    UserInterface userInterface = new UserInterface();
+
+    while(true){
+        int option = userInterface.showMenu(scanner);
+        if (option == 0)
+            break;
+
+        switch (option) {
+            //case 1 -> //show the method witch will list the books
+                
+            default -> System.out.println("Plese, select a valid option");
+        }
+    }
+
     }
 }
---
+++ Gestao Biblioteca/src/UserInterface.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UserInterface {
+
+    Scanner scanner = new Scanner(System.in);
+
+    final String MENU = """
+            \n========== Menu ===========
+            1 - List books
+            2 - Borrow a book
+            3 - Return a book
+            4 - Check the book avaliability
+            0 - Quit
+            """;
+
+    public int showMenu (Scanner scanner){
+        System.out.println(MENU);
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        return option;
+    }       
+}
+
