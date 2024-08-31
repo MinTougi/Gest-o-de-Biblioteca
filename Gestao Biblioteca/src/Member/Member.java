f6084fb0825929f060d36b220c92c60827f5f662
MinTougi <rafaela.gaievski30@gmail.com>
2024-08-31 16:47:47 -0300

    Member subclasses
    Added the sublasses from the class Member

--- Gestao Biblioteca/src/Member/Member.java
+++ Gestao Biblioteca/src/Member/Member.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 public class Member {
     private String member_id;
     private String member_name;
-    ArrayList<> 
+    private ArrayList<String> history = new ArrayList<>();
 }
