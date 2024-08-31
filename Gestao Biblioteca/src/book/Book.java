23539c9380867bca58734dae02498f7b2d949dd6
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-08-31 17:08:28 -0300

    fix book

--- Gestao Biblioteca/src/book/Book.java
+++ Gestao Biblioteca/src/book/Book.java
@@ -2,8 +2,6 @@ package book;
 
 import Author.Author;
 
-import java.time.LocalDate;
-
 public class Book extends Author {
     Author author = new Author();
 
@@ -11,40 +9,13 @@ public class Book extends Author {
     private String ISBN;
     private Boolean available;
 
-    public Book(String title, Author author, String ISBN, Boolean available) {
+    public Book(String titulo, Author author, String ISBN, Boolean available) {
         this.title = title;
         this.author = author;
-
-        // Validate ISBN format (assuming a specific format)
         if (ISBN.length() != 13 || !ISBN.matches("[0-9]+")) {
             throw new IllegalArgumentException("Invalid ISBN format");
         }
         this.ISBN = ISBN;
-
-        this.available = available;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public void setISBN(String ISBN) {
-        this.ISBN = ISBN;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
         this.available = available;
     }
 }
