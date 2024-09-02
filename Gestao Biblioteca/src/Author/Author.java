3b3b9cdb2beb7dd0a8c7cf3df910d5a6f97f7358
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-09-02 17:19:19 -0300

    Update Author.java
    Create a list of authors

--- Gestao Biblioteca/src/Author/Author.java
+++ Gestao Biblioteca/src/Author/Author.java
@@ -2,6 +2,8 @@ package Author;
 import book.Book;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Author {
     private String author_name;
@@ -41,6 +43,20 @@ public class Author {
 
     public void setBirth_data(LocalDate birth_data) {
         this.birth_data = birth_data;
+    }
+
+    private static final List<Author> authors = new ArrayList<>();
+    public void addAuthor(Author author) {
+        authors.add(author); // Add the author directly to the list
+    }
+    public static Author findAuthorByName(String name) {
+        // Assuming you have a list of Author objects stored in a static field
+        for (Author author : authors) {
+            if (author.getAuthor_name().equalsIgnoreCase(name)) {
+                return author;
+            }
+        }
+        return null;
     }
     @Override
     public String toString() {
