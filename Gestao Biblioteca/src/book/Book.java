11829810f55fcfbeabb250e3d396a93cc4fa8d80
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-09-02 17:19:34 -0300

    Update Book.java
    Checks the availability of the book

--- Gestao Biblioteca/src/book/Book.java
+++ Gestao Biblioteca/src/book/Book.java
@@ -1,20 +1,25 @@
 package book;
 
 import Author.Author;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
-
-
 public class Book extends Author {
-    Author author = new Author();
+     Author author = new Author();
      String title;
      String ISBN;
      Boolean available;
 
+    public String getTitle() {
+        return title;
+    }
 
+    public Boolean getAvailable() {
+        return available;
+    }
+    public Author getAuthor() {
+        return author;
+    }
     public Book(String title, Author author, String ISBN, Boolean available) {
         this.title = title;
         this.author = author;
@@ -24,22 +29,43 @@ public class Book extends Author {
         this.ISBN = ISBN;
         this.available = available;
     }
+    public String getAvailability() {
+        return available ? "available" : "unavailable";
+    }
     public static class Library {
-        private static List<Book> books = new ArrayList<>();
+        private static final List<Book> books = new ArrayList<>();
         public void addBook(Book book) {
-            books.add(book); // Method to add books to the list (optional)
+            books.add(book); // Method to add books to the list
         }
+
         public static void listBook(){
             for (Book book : books) {
                 System.out.println("Book Title: " + book.title);
                 System.out.println("Author: " + book.author);
                 System.out.println("ISBN: " + book.ISBN);
-                System.out.println("Available: " + book.available);
+                System.out.println("Available: " + book.getAvailability());
                 System.out.println("\n--------\n");
             }
         }
+
+        public static Book checkAvailability(String titleToCheck) {
+            for (Book book : books) {
+                if (book.getTitle().equalsIgnoreCase(book.title) && !book.getAvailable()) {
+                    return book; // Book found and available
+                }
+            }
+            return null; // Book not found or unavailable
         }
 
+//        public static Book getBookByAuthor(Author author) {
+//            for (Book book : books) {
+//                if (book.author.equals(author)) {
+//                    return book;
+//                }
+//            }
+//            return null;
+//        }
+    }
 }
 
 
