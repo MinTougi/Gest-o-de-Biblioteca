2ab9bd82833f6d7d44e4d6b526749a7689963725
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-09-02 14:42:12 -0300

    Update Book.java

--- Gestao Biblioteca/src/book/Book.java
+++ Gestao Biblioteca/src/book/Book.java
@@ -2,12 +2,18 @@ package book;
 
 import Author.Author;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
 public class Book extends Author {
     Author author = new Author();
+     String title;
+     String ISBN;
+     Boolean available;
 
-    private String title;
-    private String ISBN;
-    private Boolean available;
 
     public Book(String title, Author author, String ISBN, Boolean available) {
         this.title = title;
@@ -18,5 +24,22 @@ public class Book extends Author {
         this.ISBN = ISBN;
         this.available = available;
     }
+    public static class Library {
+        private static List<Book> books = new ArrayList<>();
+        public void addBook(Book book) {
+            books.add(book); // Method to add books to the list (optional)
+        }
+        public static void listBook(){
+            for (Book book : books) {
+                System.out.println("Book Title: " + book.title);
+                System.out.println("Author: " + book.author);
+                System.out.println("ISBN: " + book.ISBN);
+                System.out.println("Available: " + book.available);
+                System.out.println("\n--------\n");
+            }
+        }
+        }
+
 }
+
 
