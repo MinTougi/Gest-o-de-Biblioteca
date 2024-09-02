d830a35b1845f040fec0fb254a34ec7e77b17c0f
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-09-02 17:18:59 -0300

    Update to menu Case 4
    Add the method to check availabillity of a book

--- Gestao Biblioteca/src/App.java
+++ Gestao Biblioteca/src/App.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 import Author.Author;
 import book.Book;
 
@@ -6,15 +6,24 @@ import java.time.LocalDate;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-
         Scanner scanner = new Scanner (System.in);
         UserInterface userInterface = new UserInterface();
         Book.Library library = new Book.Library();
-        LocalDate LocalDate = null;
-        Author author1 = new Author ("Claudio", "Brasil", LocalDate);
-        library.addBook(new Book("Book Title 1", author1, "1234567890112", true));
-        library.addBook(new Book("Book Title 1", author1, "1234567890112", true));
+        Book.Library checkAvailability = new Book.Library();
 
+        // Data save
+        LocalDate localDate1 = LocalDate.of(1903, 07, 22);;
+        LocalDate localDate2 = LocalDate.of(1984, 12, 03);;
+        LocalDate localDate3 = LocalDate.of(1700, 03, 18);;
+        Author author1 = new Author ("Claudio", "Brasil", localDate1);
+        Author author2 = new Author ("Pedro", "Brasil", localDate2);
+        Author author3 = new Author ("Robert", "França", localDate3);
+        library.addBook(new Book("Book Title 1", author1, "1234327890112", true));
+        library.addBook(new Book("Book Title 2", author2, "1234567895512", false));
+        library.addBook(new Book("Book Title 3", author3, "4234567815512", true));
+
+
+        // Case check -> Menu
     while(true){
         int option = userInterface.showMenu(scanner);
         if (option == 0) 
@@ -22,17 +31,29 @@ public class App {
         switch (option) {
             //case 1 -> //show the method witch will list the books
             case 1:
-
                 Book.Library.listBook();
+                System.out.println("Press enter to Return to menu");
                 break;
             //case 2 -> //use the method witch will borrow a book
             case 2:
+
                 break;
             //case 3 -> //use the method witch will return a book
             case 3:
                 break;
-            //case 4 -> //use the method witch will check the avaliability of a book
+            //case 4 -> //use the method witch will check the availability of a book
             case 4:
+                System.out.println("Enter the title of the book you want to check:");
+                String titleToCheck = scanner.nextLine();
+                Book availableBook = Book.Library.checkAvailability(titleToCheck);
+                if (availableBook != null) {
+                    System.out.println("Book found!");
+                    System.out.println("Title: " + availableBook.getTitle());
+                    System.out.println("Author: " + availableBook.getAuthor());
+                    System.out.println("Available: " + availableBook.getAvailability());
+                } else {
+                    System.out.println("Book not found or unavailable.");
+                }
                 break;
             //case 5 -> //Finish the running code
             case 5:
