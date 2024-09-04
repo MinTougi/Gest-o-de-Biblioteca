c4395db81b0162708e5d4eca74bfadbf158fbbc9
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-09-03 22:48:23 -0300

    Update Professor.java

--- Gestao Biblioteca/src/Member/Professor.java
+++ Gestao Biblioteca/src/Member/Professor.java
@@ -2,4 +2,24 @@ package Member;
 
 public class Professor extends Member {
     private String departament;
+
+    public Professor(String member_name, String member_id, String departament) {
+        super(member_name, member_id);
+        this.departament = departament;
+    }
+
+    public String getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(String departament) {
+        this.departament = departament;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nDepartment: " + departament;
+    }
+
+
 }
