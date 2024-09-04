b5920ae47ac74ce5b2cefc48cf52b550e4f2f269
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-09-03 22:48:24 -0300

    Update Student.java

--- Gestao Biblioteca/src/Member/Student.java
+++ Gestao Biblioteca/src/Member/Student.java
@@ -1,5 +1,18 @@
 package Member;
 
-public class Student extends Member{
-    private String courses;
+public class Student extends Member {
+    private String course;
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public Student(String memberName, String memberId, String course) {
+        super(memberName, memberId);
+        this.course = course;
+    }
 }
